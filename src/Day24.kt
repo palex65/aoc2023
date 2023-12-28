65c9e207bb9893c0e53f33907881a21c3b2507b9
Pedro Pereira <593033+palex65@users.noreply.github.com>
2023-12-28 22:32:36 +0000

    Update Day24

--- src/Day24.kt
+++ src/Day24.kt
@@ -4,80 +4,77 @@ package day24
 
 import println
 import readInput
-import kotlin.math.roundToInt
 import kotlin.math.roundToLong
 import kotlin.time.measureTime
+import day24.Axis.*
 
 typealias Input = List<String>
 
-data class Line(val x: Long, val y: Long, val z: Long, val dx: Int, val dy: Int, val dz: Int)
+enum class Axis { X, Y, Z }
+val idxAxis = Axis.entries.map { it.ordinal }
+
+data class Line(val pos: List<Long>, val vel: List<Int>)
+val Line.dx get() = vel[X.ordinal]
+val Line.dy get() = vel[Y.ordinal]
+val Line.x get() = pos[X.ordinal]
+val Line.y get() = pos[Y.ordinal]
+
+fun Line.at(axis: Axis, t: Float) = pos[axis.ordinal] + vel[axis.ordinal] * t
+fun Line.at(axis: Axis, t: Long) = pos[axis.ordinal] + vel[axis.ordinal] * t
+
+fun Line.isFuture(axis: Axis, value: Float) =
+    pos[axis.ordinal].let { p -> vel[axis.ordinal].let { v -> v > 0 && p < value || v < 0 && p > value } }
 
 fun Input.parseLines() = map { line ->
     val (pos,vel) = line.split(" @ ").map { it.split(", ") }
-    val (x,y,z) = pos.map { it.trim().toLong() }
-    val (dx,dy,dz) = vel.map { it.trim().toInt() }
-    Line(x,y,z,dx,dy,dz)
+    Line( pos.map { it.trim().toLong() }, vel.map { it.trim().toInt() } )
+}
+
+inline fun <T> List<T>.countForEachPair(predicate: (T,T) -> Boolean): Int {
+    var count = 0
+    for (i in 0..size-2) for (j in i+1 ..< size) if (predicate(this[i],this[j])) count++
+    return count
 }
 
 fun main() {
-    fun part1(input: Input, range: LongRange): Int {
-        val lines = input.parseLines()
-        var count = 0
-        for (i in 0..lines.size-2) {
-            val l1 = lines[i]
-            for (j in i+1 ..< lines.size) {
-                val l2 = lines[j]
-                val den = l2.dx * l1.dy - l1.dx * l2.dy
-                if (den==0) continue  // parallel lines
-                val s = ( l1.dx * (l2.y - l1.y) - l1.dy * (l2.x - l1.x) ) / den.toFloat()
-                val x = l2.x + l2.dx * s
-                val y = l2.y + l2.dy * s
-                if (
-                    (l1.dx>0 && x < l1.x || l1.dx<0 && x > l1.x || l1.dy>0 && y < l1.y || l1.dy<0 && y > l1.y) // Past for l1
-                ||  (l2.dx>0 && x < l2.x || l2.dx<0 && x > l2.x || l2.dy>0 && y < l2.y || l2.dy<0 && y > l2.y) // Past for l2
-                ||  (x.roundToLong() !in range || y.roundToLong() !in range) // Out of range
-                   ) continue
-                //println("$l1 ++ $l2 -> x=$x, y=$y")
-                count++
+    fun part1(input: Input, range: LongRange): Int =
+        input.parseLines().countForEachPair { l1, l2 ->
+            val den = l2.dx * l1.dy - l1.dx * l2.dy
+            if (den==0) false  // Parallel lines
+            else {
+                val s = (l1.dx * (l2.y - l1.y) - l1.dy * (l2.x - l1.x)) / den.toFloat()
+                val x = l2.at(X, s)
+                val y = l2.at(Y, s)
+                l1.isFuture(X, x) && l1.isFuture(Y, y) && l2.isFuture(X, x) && l2.isFuture(Y, y)  // Past for l1 or l2
+                    && x.roundToLong() in range && y.roundToLong() in range        // Out of range
             }
         }
-        return count
-    }
 
     fun part2(input: Input): Long {
         val lines = input.parseLines()
-        fun computeAxis(ns: List<Long>, ds: List<Int>): Long {
-            val minD = -10
-            val maxD = +10
-            val maxN = ns.maxOrNull()!! + maxD
-            fun computeInterceptTime(i: Int, n: Long, d: Int): Int? {
-                val den = d-ds[i]
-                if (den==0) return null
-                val t = (ns[i]-n).toFloat() / den
-                val ti = t.toInt()
-                return if (t >= 0 && t.roundToInt() == ti) ti else null
-            }
-            fun intercepts(i: Int, n: Long, d: Int) = computeInterceptTime(i,n,d) != null
-            for(n in 0..maxN)
-                for(d in minD..maxD) if ( ns.indices.all { intercepts(it,n,d) } ) {
-                    val ts = ns.indices.map { computeInterceptTime(it,n,d)!! }
-                    if (ts.toSet().size == ts.size && ts.withIndex().all { (i,t) -> n+d*t == ns[i]+ds[i]*t })
-                        return n
-                }
-            return -1L
+        val times = idxAxis.firstNotNullOf { a ->
+            lines.firstNotNullOfOrNull lab@ { l1 -> lines.map { l2 ->
+                val tn = l2.pos[a] - l1.pos[a]
+                val td = l1.vel[a] - l2.vel[a]
+                if (td==0) if (tn==0L) 0L else return@lab null
+                else if (tn % td == 0L) tn / td else return@lab null
+            } }
         }
-
-        val x = computeAxis(lines.map { it.x }, lines.map { it.dx })
-        val y = computeAxis(lines.map { it.y }, lines.map { it.dy })
-        val z = computeAxis(lines.map { it.z }, lines.map { it.dz })
-        return x + y + z
+        val linesTimes = times.withIndex().filter { it.value > 0 }.take(2).map { lines[it.index] to it.value }
+        val (line1,time1) = linesTimes[0]
+        val (line2,time2) = linesTimes[1]
+        return Axis.entries.sumOf { axis ->
+            val p1 = line1.at(axis,time1)
+            val p2 = line2.at(axis,time2)
+            p1 - (p1 - p2) / (time1 - time2) * time1
+        }
     }
 
     val testInput = readInput("Day24_test")
     check(part1(testInput, 7L..27) == 2)
-//    check(part2(testInput) == 47L)
+    check(part2(testInput) == 47L)
 //
     val input = readInput("Day24")
-    println( measureTime { part1(input, 200000000000000..400000000000000).println() } ) // 13910 - 20ms
-//    println( measureTime { part2(input).println() } ) // 1 - 50ms
+    println( measureTime { part1(input, 200000000000000..400000000000000).println() } ) // 13910 - 25ms
+    println( measureTime { part2(input).println() } ) // 618534564836937 - 2ms
 }
